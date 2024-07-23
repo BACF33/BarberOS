ba0310d618fd3ed642ce5e370425c12f26846f01
Moyitag <20240099@ricaldone.edu.sv>
2024-07-23 10:56:28 -0600

    bueno mira te cambie la vista y tambien de recompensas

--- BarberOS/Properties/Resources.Designer.cs
+++ BarberOS/Properties/Resources.Designer.cs
@@ -133,6 +133,16 @@ namespace BarberOS.Properties {
         /// <summary>
         ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Captura21 {
+            get {
+                object obj = ResourceManager.GetObject("Captura21", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Captura3 {
             get {
                 object obj = ResourceManager.GetObject("Captura3", resourceCulture);
