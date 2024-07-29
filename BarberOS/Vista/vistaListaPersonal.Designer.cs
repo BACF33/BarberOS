1e8d815666d3d128136f5b48a3fe4635d6a3b140
BACF33 <20210181@ricaldone.edu.sv>
2024-07-28 23:25:59 -0600

    Test

---
+++ BarberOS/Vista/vistaListaPersonal.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace BarberOS.Vista
+{
+    partial class vistaListaPersonal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flpPersonal = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // flpPersonal
+            // 
+            this.flpPersonal.Location = new System.Drawing.Point(12, 12);
+            this.flpPersonal.Name = "flpPersonal";
+            this.flpPersonal.Size = new System.Drawing.Size(776, 416);
+            this.flpPersonal.TabIndex = 0;
+            // 
+            // vistaListaPersonal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.flpPersonal);
+            this.Name = "vistaListaPersonal";
+            this.Text = "vistaListaClientes";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flpPersonal;
+    }
+}
---
+++ BarberOS/Vista/vistaListaPersonal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BarberOS.Vista;
+
+namespace BarberOS.Vista
+{
+    public partial class vistaListaPersonal : Form
+    {
+        private vistaMenu mainForm;
+        vistaListaPersonalG fuenteInfoPersonal;
+
+        public vistaListaPersonal()
+        {
+            InitializeComponent();
+            fuenteInfoPersonal = new vistaListaPersonalG(mainForm);
+        }
+
+        private void cargarProductos()
+        {
+        }
+    }
+}
