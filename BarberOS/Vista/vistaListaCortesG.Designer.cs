38f12ed57e32b9ee2417b5c2854d9049907f05b2
BACF33 <20210181@ricaldone.edu.sv>
2024-09-10 19:22:22 -0600

    extra

--- BarberOS/Vista/vistaListaCortesG.Designer.cs
+++ BarberOS/Vista/vistaListaCortesG.Designer.cs
@@ -376,6 +376,7 @@
             // 
             // cmbTipo
             // 
+            this.cmbTipo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbTipo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cmbTipo.FormattingEnabled = true;
             this.cmbTipo.Items.AddRange(new object[] {
