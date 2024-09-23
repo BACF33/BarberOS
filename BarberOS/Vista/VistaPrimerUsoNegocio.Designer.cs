fa1fd99926a743d375c9ec07a5dfe863379fc8a7
Robbie Larios <robbieavellana@gmail.com>
2024-09-23 11:29:22 -0600

    Mas

--- BarberOS/Vista/VistaPrimerUsoNegocio.Designer.cs
+++ BarberOS/Vista/VistaPrimerUsoNegocio.Designer.cs
@@ -30,23 +30,23 @@
         {
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.txtLema = new System.Windows.Forms.TextBox();
-            this.txtNuevoNombre = new System.Windows.Forms.TextBox();
-            this.txtEmail = new System.Windows.Forms.TextBox();
-            this.btnEnviar = new System.Windows.Forms.Button();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
             this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.txtNuevoNombre = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtLema = new System.Windows.Forms.TextBox();
+            this.btnEnviar = new System.Windows.Forms.Button();
             this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.tableLayoutPanel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -54,8 +54,8 @@
             this.tableLayoutPanel1.ColumnCount = 2;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.pictureBox3, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
@@ -77,29 +77,55 @@
             this.pictureBox3.TabIndex = 65;
             this.pictureBox3.TabStop = false;
             // 
-            // label4
+            // tableLayoutPanel2
             // 
-            this.label4.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.ForeColor = System.Drawing.Color.White;
-            this.label4.Location = new System.Drawing.Point(204, 334);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(64, 20);
-            this.label4.TabIndex = 9;
-            this.label4.Text = "E-mail:";
+            this.tableLayoutPanel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(64)))), ((int)(((byte)(103)))));
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 22.68041F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 77.31959F));
+            this.tableLayoutPanel2.Controls.Add(this.txtNuevoNombre, 1, 1);
+            this.tableLayoutPanel2.Controls.Add(this.pictureBox4, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.label1, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label4, 1, 2);
+            this.tableLayoutPanel2.Controls.Add(this.pictureBox7, 0, 3);
+            this.tableLayoutPanel2.Controls.Add(this.txtEmail, 1, 3);
+            this.tableLayoutPanel2.Controls.Add(this.pictureBox1, 0, 5);
+            this.tableLayoutPanel2.Controls.Add(this.label3, 1, 4);
+            this.tableLayoutPanel2.Controls.Add(this.txtLema, 1, 5);
+            this.tableLayoutPanel2.Controls.Add(this.btnEnviar, 1, 6);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 7;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28572F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28572F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28572F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28572F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28572F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28572F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(679, 743);
+            this.tableLayoutPanel2.TabIndex = 66;
             // 
-            // label3
+            // pictureBox4
             // 
-            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(204, 413);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(53, 20);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "Lema";
+            this.pictureBox4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox4.Cursor = System.Windows.Forms.Cursors.Default;
+            this.pictureBox4.Image = global::BarberOS.Properties.Resources._9131529;
+            this.pictureBox4.Location = new System.Drawing.Point(28, 111);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(97, 96);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 111;
+            this.pictureBox4.TabStop = false;
+            // 
+            // txtNuevoNombre
+            // 
+            this.txtNuevoNombre.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtNuevoNombre.Location = new System.Drawing.Point(307, 149);
+            this.txtNuevoNombre.Name = "txtNuevoNombre";
+            this.txtNuevoNombre.Size = new System.Drawing.Size(217, 20);
+            this.txtNuevoNombre.TabIndex = 112;
             // 
             // label1
             // 
@@ -107,35 +133,74 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(202, 174);
+            this.label1.Location = new System.Drawing.Point(327, 43);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(178, 20);
-            this.label1.TabIndex = 0;
+            this.label1.TabIndex = 113;
             this.label1.Text = "Nombre Del Negocio:";
             // 
-            // txtLema
+            // label4
             // 
-            this.txtLema.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.txtLema.Location = new System.Drawing.Point(206, 451);
-            this.txtLema.Name = "txtLema";
-            this.txtLema.Size = new System.Drawing.Size(217, 20);
-            this.txtLema.TabIndex = 12;
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(384, 255);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 20);
+            this.label4.TabIndex = 114;
+            this.label4.Text = "E-mail:";
             // 
-            // txtNuevoNombre
+            // pictureBox7
             // 
-            this.txtNuevoNombre.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.txtNuevoNombre.Location = new System.Drawing.Point(206, 200);
-            this.txtNuevoNombre.Name = "txtNuevoNombre";
-            this.txtNuevoNombre.Size = new System.Drawing.Size(217, 20);
-            this.txtNuevoNombre.TabIndex = 10;
+            this.pictureBox7.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox7.Cursor = System.Windows.Forms.Cursors.Default;
+            this.pictureBox7.Image = global::BarberOS.Properties.Resources.Email_IconPNG;
+            this.pictureBox7.Location = new System.Drawing.Point(35, 328);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(82, 86);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox7.TabIndex = 115;
+            this.pictureBox7.TabStop = false;
             // 
             // txtEmail
             // 
             this.txtEmail.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.txtEmail.Location = new System.Drawing.Point(206, 369);
+            this.txtEmail.Location = new System.Drawing.Point(307, 361);
             this.txtEmail.Name = "txtEmail";
             this.txtEmail.Size = new System.Drawing.Size(217, 20);
-            this.txtEmail.TabIndex = 13;
+            this.txtEmail.TabIndex = 116;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox1.Image = global::BarberOS.Properties.Resources.telefono1;
+            this.pictureBox1.Location = new System.Drawing.Point(41, 544);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(70, 77);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 117;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(389, 467);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 20);
+            this.label3.TabIndex = 118;
+            this.label3.Text = "Lema";
+            // 
+            // txtLema
+            // 
+            this.txtLema.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtLema.Location = new System.Drawing.Point(307, 573);
+            this.txtLema.Name = "txtLema";
+            this.txtLema.Size = new System.Drawing.Size(217, 20);
+            this.txtLema.TabIndex = 119;
             // 
             // btnEnviar
             // 
@@ -145,66 +210,12 @@
             this.btnEnviar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnEnviar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnEnviar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.btnEnviar.Location = new System.Drawing.Point(213, 499);
+            this.btnEnviar.Location = new System.Drawing.Point(279, 639);
             this.btnEnviar.Name = "btnEnviar";
-            this.btnEnviar.Size = new System.Drawing.Size(167, 75);
-            this.btnEnviar.TabIndex = 70;
+            this.btnEnviar.Size = new System.Drawing.Size(273, 101);
+            this.btnEnviar.TabIndex = 120;
             this.btnEnviar.Text = "Enviar";
             this.btnEnviar.UseVisualStyleBackColor = false;
-            // 
-            // pictureBox4
-            // 
-            this.pictureBox4.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.pictureBox4.Cursor = System.Windows.Forms.Cursors.Default;
-            this.pictureBox4.Image = global::BarberOS.Properties.Resources._9131529;
-            this.pictureBox4.Location = new System.Drawing.Point(139, 174);
-            this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(44, 46);
-            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox4.TabIndex = 67;
-            this.pictureBox4.TabStop = false;
-            // 
-            // pictureBox7
-            // 
-            this.pictureBox7.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.pictureBox7.Cursor = System.Windows.Forms.Cursors.Default;
-            this.pictureBox7.Image = global::BarberOS.Properties.Resources.Email_IconPNG;
-            this.pictureBox7.Location = new System.Drawing.Point(139, 334);
-            this.pictureBox7.Name = "pictureBox7";
-            this.pictureBox7.Size = new System.Drawing.Size(44, 46);
-            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox7.TabIndex = 102;
-            this.pictureBox7.TabStop = false;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.pictureBox1.Image = global::BarberOS.Properties.Resources.telefono1;
-            this.pictureBox1.Location = new System.Drawing.Point(139, 413);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(44, 46);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 103;
-            this.pictureBox1.TabStop = false;
-            // 
-            // panel1
-            // 
-            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(64)))), ((int)(((byte)(103)))));
-            this.panel1.Controls.Add(this.pictureBox1);
-            this.panel1.Controls.Add(this.pictureBox7);
-            this.panel1.Controls.Add(this.pictureBox4);
-            this.panel1.Controls.Add(this.btnEnviar);
-            this.panel1.Controls.Add(this.txtEmail);
-            this.panel1.Controls.Add(this.txtNuevoNombre);
-            this.panel1.Controls.Add(this.txtLema);
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.label3);
-            this.panel1.Controls.Add(this.label4);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(3, 3);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(679, 743);
-            this.panel1.TabIndex = 14;
             // 
             // VistaPrimerUsoNegocio
             // 
@@ -218,11 +229,11 @@
             this.Text = "VistaPrimerUsoNegocio";
             this.tableLayoutPanel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -230,17 +241,17 @@
         #endregion
 
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox7;
-        private System.Windows.Forms.PictureBox pictureBox4;
-        public System.Windows.Forms.Button btnEnviar;
-        public System.Windows.Forms.TextBox txtEmail;
-        public System.Windows.Forms.TextBox txtNuevoNombre;
-        public System.Windows.Forms.TextBox txtLema;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        public System.Windows.Forms.TextBox txtNuevoNombre;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        public System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        public System.Windows.Forms.TextBox txtLema;
+        public System.Windows.Forms.Button btnEnviar;
     }
 }
--- BarberOS/Vista/vistaPerfilUsuario.Designer.cs
+++ BarberOS/Vista/vistaPerfilUsuario.Designer.cs
@@ -28,114 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pnlBarberos = new System.Windows.Forms.Panel();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.btnBarberos = new System.Windows.Forms.Button();
-            this.pnlAgendar = new System.Windows.Forms.Panel();
-            this.pictureBox6 = new System.Windows.Forms.PictureBox();
-            this.btnAgendar = new System.Windows.Forms.Button();
-            this.pnlBarberos.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            this.pnlAgendar.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
             this.SuspendLayout();
-            // 
-            // pnlBarberos
-            // 
-            this.pnlBarberos.Controls.Add(this.pictureBox2);
-            this.pnlBarberos.Controls.Add(this.btnBarberos);
-            this.pnlBarberos.Location = new System.Drawing.Point(424, 301);
-            this.pnlBarberos.Name = "pnlBarberos";
-            this.pnlBarberos.Size = new System.Drawing.Size(270, 71);
-            this.pnlBarberos.TabIndex = 113;
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(28)))), ((int)(((byte)(45)))));
-            this.pictureBox2.Image = global::BarberOS.Properties.Resources.Proyecto_nuevo__8_;
-            this.pictureBox2.Location = new System.Drawing.Point(10, 10);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(10, 10, 3, 3);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(50, 44);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox2.TabIndex = 110;
-            this.pictureBox2.TabStop = false;
-            // 
-            // btnBarberos
-            // 
-            this.btnBarberos.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(28)))), ((int)(((byte)(45)))));
-            this.btnBarberos.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btnBarberos.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnBarberos.ForeColor = System.Drawing.SystemColors.Control;
-            this.btnBarberos.Location = new System.Drawing.Point(0, 2);
-            this.btnBarberos.Margin = new System.Windows.Forms.Padding(3, 16, 3, 3);
-            this.btnBarberos.Name = "btnBarberos";
-            this.btnBarberos.Size = new System.Drawing.Size(281, 66);
-            this.btnBarberos.TabIndex = 102;
-            this.btnBarberos.TabStop = false;
-            this.btnBarberos.Text = "     BARBEROS";
-            this.btnBarberos.UseVisualStyleBackColor = false;
-            // 
-            // pnlAgendar
-            // 
-            this.pnlAgendar.Controls.Add(this.pictureBox6);
-            this.pnlAgendar.Controls.Add(this.btnAgendar);
-            this.pnlAgendar.Location = new System.Drawing.Point(400, 150);
-            this.pnlAgendar.Name = "pnlAgendar";
-            this.pnlAgendar.Size = new System.Drawing.Size(281, 60);
-            this.pnlAgendar.TabIndex = 117;
-            // 
-            // pictureBox6
-            // 
-            this.pictureBox6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(28)))), ((int)(((byte)(45)))));
-            this.pictureBox6.Image = global::BarberOS.Properties.Resources._52462901;
-            this.pictureBox6.Location = new System.Drawing.Point(10, 10);
-            this.pictureBox6.Margin = new System.Windows.Forms.Padding(10, 10, 3, 3);
-            this.pictureBox6.Name = "pictureBox6";
-            this.pictureBox6.Size = new System.Drawing.Size(50, 46);
-            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox6.TabIndex = 114;
-            this.pictureBox6.TabStop = false;
-            // 
-            // btnAgendar
-            // 
-            this.btnAgendar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(28)))), ((int)(((byte)(45)))));
-            this.btnAgendar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btnAgendar.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAgendar.ForeColor = System.Drawing.SystemColors.Control;
-            this.btnAgendar.Location = new System.Drawing.Point(3, 3);
-            this.btnAgendar.Margin = new System.Windows.Forms.Padding(3, 16, 3, 3);
-            this.btnAgendar.Name = "btnAgendar";
-            this.btnAgendar.Size = new System.Drawing.Size(270, 54);
-            this.btnAgendar.TabIndex = 105;
-            this.btnAgendar.TabStop = false;
-            this.btnAgendar.Text = "      AGENDAR";
-            this.btnAgendar.UseVisualStyleBackColor = false;
             // 
             // VistaPerfilUsuario
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1118, 673);
-            this.Controls.Add(this.pnlAgendar);
-            this.Controls.Add(this.pnlBarberos);
             this.Name = "VistaPerfilUsuario";
             this.Text = "vistaPerfilUsuario";
-            this.pnlBarberos.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            this.pnlAgendar.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        public System.Windows.Forms.Panel pnlBarberos;
-        private System.Windows.Forms.PictureBox pictureBox2;
-        public System.Windows.Forms.Button btnBarberos;
-        public System.Windows.Forms.Panel pnlAgendar;
-        private System.Windows.Forms.PictureBox pictureBox6;
-        public System.Windows.Forms.Button btnAgendar;
     }
 }
